d87cd1207249b23c3b44358d1b2583edffcc7016
AlkanCH <227445@student.uek.krakow.pl>
2023-06-14 13:02:22 +0200

    2023-06-14 AZ

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,26 @@
 from app import app
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Aleksander Ziętkiewicz!"
+    return render_template('index.html')
 
-@app.route('/name/',defaults={'name': "Anonim"})
-@app.route('/name/<name>')
-def name(name=None):
-    return f"Hello, {name}!"
+@app.route('/extract', methods=['POST',  'GET'])
+def extract():
+    if request.method == 'POST':
+        product_code = request.form.get['product_id']
+        return redirect(url_for('product', product_code=product_code))
+    return render_template('extract.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/product/<product_code>')
+def product(product_code):
+    return render_template('product.html', product_code=product_code)
+
+@app.route('/author')
+def author():
+    return render_template('author.html')
